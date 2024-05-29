11c7484a82741e65274eca5841015ed4f7d1df16
Tikthisweek <165034941+Tikthisweek@users.noreply.github.com>
2024-05-29 16:33:57 -0400

    Update amex_spend_analysis.py

--- amex_spend_analysis.py
+++ amex_spend_analysis.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Mar 12 14:00:14 2024
 
@@ -12,135 +10,144 @@ import os
 import openpyxl
 from openpyxl.styles import Font, Alignment, Border, Side, NamedStyle
 
-amex = pd.read_excel('/Users/KartikPatel/Downloads/Activity.xlsx',header = 6)
+monthly_statement= pd.read_excel("/Users/KartikPatel/Downloads/Activity.xlsx", header=6)
 
 
-def remove_payments(X):
-    X.loc[X['Amount']<0,'Amount']=0
-    return X
-
-amex=remove_payments(amex)
+def zero_out_payments(statement):
+    statement.loc[statement["Amount"] < 0, "Amount"] = 0
+    return statement
 
 
-def cleanup(X):
-    for name in X.Description:
-        word=name.split(' ')
-        X.loc[X['Description']==name,'Description']=word[0]
-        word=[]
-    return X
+def cleanup(statement):
+    for name in statement.Description:
+        word = name.split(" ")
+        statement.loc[statement["Description"] == name, "Description"] = word[0]
+        word = []
+    return statement
 
-amex=cleanup(amex)
+monthly_statement = zero_out_payments(monthly_statement)
+monthly_statement = cleanup(monthly_statement)
 
-def spendperday(X):
-    spendbyday=pd.DataFrame()
-    spendbyday=X.groupby('Date')['Amount'].sum().reset_index()
-    spendbyday.columns=['Date','Total_Spent']
+
+def spendperday(statement):
+    spendbyday = pd.DataFrame()
+    spendbyday = statement.groupby("Date")["Amount"].sum().reset_index()
+    spendbyday.columns = ["Date", "Total_Spent"]
     return spendbyday
 
 
-
-def spendbyvendor(X):
-    spendbyvendor=pd.DataFrame()
-    spendbyvendor=X.groupby('Description')['Amount'].sum().reset_index()
-    spendbyvendor.columns=['Vendor','Amount']
-    total_spend=spendbyvendor.Amount.sum()
-    new_row = pd.DataFrame({'Vendor':'Total_Spend', 'Amount': [total_spend]})
-    spendbyvendor=pd.concat([spendbyvendor,new_row],ignore_index=True)
+def spendbyvendor(statement):
+    spendbyvendor = pd.DataFrame()
+    spendbyvendor = statement.groupby("Description")["Amount"].sum().reset_index()
+    spendbyvendor.columns = ["Vendor", "Amount"]
+    total_spend = spendbyvendor.Amount.sum()
+    new_row = pd.DataFrame({"Vendor": "Total_Spend", "Amount": [total_spend]})
+    spendbyvendor = pd.concat([spendbyvendor, new_row], ignore_index=True)
     return spendbyvendor
+
 
 def format_numeric(x):
     if isinstance(x, (int, float)):
-        return '{:.2f}'.format(x)
+        return "{:.2f}".format(x)
     return x
 
-spendbyvendor=spendbyvendor(amex)
+
+spendbyvendor = spendbyvendor(monthly_statement)
+spendperday = spendperday(monthly_statement)
+
+spendbyvendor.sort_values(by="Amount")
+
+Excel_filepath = (
+    "/Users/KartikPatel/Desktop/Automate_The_Boring_Stuff/monthly_statement/Cycle_Summary.xlsx"
+)
+
+statement_month = monthly_statement.Date[0].split("/")[0]
+statement_year = monthly_statement.Date[0].split("/")[2]
+
+with pd.ExcelWriter(Excel_filepath, mode="a") as writer:
+    name = statement_month + "-" + statement_year + " Vendors"
+    spendbyvendor.to_excel(writer, sheet_name=name, index=False)
 
 
-spendbyvendor.sort_values(by='Amount')
+stats = monthly_statement.describe()
+stats = stats.applymap(format_numeric)
 
-Excel_filepath="/Users/KartikPatel/Desktop/Automate_The_Boring_Stuff/Amex/Cycle_Summary.xlsx"
+with pd.ExcelWriter(Excel_filepath, mode="a") as writer:
+    statname = statement_month + "-" + statement_year + " Stats"
+    stats.to_excel(writer, sheet_name=statname, index=True)
 
-statement_month=amex.Date[0].split('/')[0]
-statement_year=amex.Date[0].split('/')[2]
+with pd.ExcelWriter(Excel_filepath, mode="a") as writer:
+    name = statement_month + "-" + statement_year + " Daily Spend"
+    spendperday.to_excel(writer, sheet_name=name, index=False)
 
-with pd.ExcelWriter(Excel_filepath, mode='a') as writer:
-    name=statement_month + "-" + statement_year + " Vendors"
-    spendbyvendor.to_excel(writer, sheet_name= name, index=False)
-    
-    
-stats=amex.describe()
-stats=stats.applymap(format_numeric)
-
-with pd.ExcelWriter(Excel_filepath, mode='a') as writer:
-    statname=statement_month + "-" + statement_year +' Stats'
-    stats.to_excel(writer,sheet_name= statname,index=True)
-
-
-workbook=openpyxl.load_workbook(Excel_filepath)
-border_style=Border(left=Side(style='thin'),right=Side(style='thin'),top=Side(style='thin'),bottom=Side(style='thin'))
+workbook = openpyxl.load_workbook(Excel_filepath)
+border_style = Border(
+    left=Side(style="thin"),
+    right=Side(style="thin"),
+    top=Side(style="thin"),
+    bottom=Side(style="thin"),
+)
 
 
-vendorsheet=workbook[name]
+vendorsheet = workbook[name]
 for cell in vendorsheet[1]:
     cell.font = Font(bold=True)
 
 
-for row in vendorsheet.iter_rows(min_col=1,max_col=2):
+for row in vendorsheet.iter_rows(min_col=1, max_col=2):
     for cell in row:
-        cell.alignment = Alignment(horizontal='center',vertical='center')
+        cell.alignment = Alignment(horizontal="center", vertical="center")
         cell.border = border_style
-        
-for row in vendorsheet.iter_rows(min_row=2,min_col=2):
-    for cell in row:
-        cell.number_format='"$"#,##0.00'
-        
 
-statssheet=workbook[statname]
-statssheet.cell(row=1, column=1,value="Stat")
+for row in vendorsheet.iter_rows(min_row=2, min_col=2):
+    for cell in row:
+        cell.number_format = '"$"#,##0.00'
+
+
+statssheet = workbook[statname]
+statssheet.cell(row=1, column=1, value="Stat")
 for cell in statssheet[1]:
     cell.font = Font(bold=True)
-    
-for row in statssheet.iter_rows(min_row=2,max_row=9):
+
+for row in statssheet.iter_rows(min_row=2, max_row=9):
     for cell in row:
         cell.font = Font(bold=False)
-    
-for row in statssheet.iter_rows(min_col=1,max_col=2):
+
+for row in statssheet.iter_rows(min_col=1, max_col=2):
     for cell in row:
-        cell.alignment = Alignment(horizontal='center',vertical='center')
+        cell.alignment = Alignment(horizontal="center", vertical="center")
         cell.border = border_style
-        
-for row in statssheet.iter_rows(min_row=2,min_col=2):
+
+for row in statssheet.iter_rows(min_row=2, min_col=2):
     for cell in row:
-        cell.number_format='"$"#,##0.00'
-        
+        cell.number_format = '"$"#,##0.00'
+
 workbook.save(Excel_filepath)
-        
-plt.bar(spendbyvendor.loc[spendbyvendor['Amount']>100,'Vendor'],spendbyvendor.loc[spendbyvendor['Amount']>100,'Amount'])
-plt.xlabel('Vendor', fontsize=8)  # Adjust the font size here
-plt.ylabel('Amount ($)')
-plt.title('Amount Spent by Vendor (Amount > $100)')
-plt.xticks(rotation=45, ha='right', fontsize=8)  # Adjust the font size here
+
+plt.bar(
+    spendbyvendor.loc[spendbyvendor["Amount"] > 100, "Vendor"],
+    spendbyvendor.loc[spendbyvendor["Amount"] > 100, "Amount"],
+)
+plt.xlabel("Vendor", fontsize=8)  # Adjust the font size here
+plt.ylabel("Amount ($)")
+plt.title("Amount Spent by Vendor (Amount > $100)")
+plt.xticks(rotation=45, ha="right", fontsize=8)  # Adjust the font size here
 plt.tight_layout()  # Adjust layout to prevent labels from overlapping
 
-min_date=str(amex.Date.min())
-min_date=min_date.replace('/', '_')
-max_date=str(amex.Date.max())
-max_date=max_date.replace('/','_')
+min_date = str(monthly_statement.Date.min())
+min_date = min_date.replace("/", "_")
+max_date = str(monthly_statement.Date.max())
+max_date = max_date.replace("/", "_")
 
 
-folder_path = '/Users/KartikPatel/Desktop/Automate_The_Boring_Stuff/Amex/Amex_Spend_Graphs'
-file_name=min_date + ' to ' + max_date +'.png'
-file_path=folder_path + '/' + file_name
+folder_path = (
+    "/Users/KartikPatel/Desktop/Automate_The_Boring_Stuff/monthly_statement/monthly_statement_spend_graphs"
+)
+file_name = min_date + " to " + max_date + ".png"
+file_path = folder_path + "/" + file_name
 plt.savefig(file_path)
 
 
 os.system(f"open {Excel_filepath}")
-remove_file='/Users/KartikPatel/Downloads/Activity.xlsx'
+remove_file = "/Users/KartikPatel/Downloads/Activity.xlsx"
 os.remove(remove_file)
-    
-
-    
-    
-    
-    
-    
